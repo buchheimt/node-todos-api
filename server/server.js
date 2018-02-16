4a31b9ca3f1207f6ce07c88c4fcbe299869271d1
Tyler <tbuchhei@alumni.nd.edu>
2018-02-15 19:52:34 -0600

    Add basic validations to User and Todo models

--- server/server.js
+++ server/server.js
@@ -5,18 +5,32 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+})
+
 const newTodo = new Todo({
-  text: 'Eat lunch'
+  text: 'Testing 123'
 });
 
 newTodo.save()
@@ -37,4 +51,13 @@ myTodo.save()
     console.log('Saved my todo:', doc);
   }, err => {
     console.log('Unable to save Todo');
+  });
+
+const newUser = new User({email: 'test@testytest.net'});
+
+newUser.save()
+  .then(doc => {
+    console.log(JSON.stringify(doc, undefined, 1));
+  }, e => {
+    console.log("Unable to save user:", e);
   });
