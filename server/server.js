d641f081d8d4f8b0f0b728208c450a67de44de2d
Tyler <tbuchhei@alumni.nd.edu>
2018-02-18 07:53:37 -0600

    Implement Todo#index route

--- server/server.js
+++ server/server.js
@@ -19,9 +19,18 @@ app.post('/todos', (req, res) => {
   todo.save()
     .then(doc => {
       res.send(doc);
-    }, e => {
-      res.status(400).send(e);
+    }, err => {
+      res.status(400).send(err);
     });  
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({todos});
+    }, err => {
+      res.status(400).send(err)
+    });
 });
 
 // Run
