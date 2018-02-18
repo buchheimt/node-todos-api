040434778cc86602ee84d0c5c3e708bbcdab9ffb
Tyler <tbuchhei@alumni.nd.edu>
2018-02-18 09:19:48 -0600

    Implement Todo#show route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
+const {ObjectId} = require('mongodb');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
@@ -31,6 +32,24 @@ app.get('/todos', (req, res) => {
     }, err => {
       res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      
+      res.send({todo});
+    })
+    .catch(err => res.status(400).send());
 });
 
 // Run
