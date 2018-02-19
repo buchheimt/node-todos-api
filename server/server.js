617ed0408988bc7748b73abf0936082080dd4a8f
Tyler <tbuchhei@alumni.nd.edu>
2018-02-19 16:52:17 -0600

    Implement Todo#destroy route

--- server/server.js
+++ server/server.js
@@ -53,6 +53,24 @@ app.get('/todos/:id', (req, res) => {
     .catch(err => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    })
+    .catch(err => res.status(400).send());
+});
+
 // Run
 
 app.listen(port, () => {
